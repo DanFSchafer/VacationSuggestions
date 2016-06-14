879b162fcf86fd45931a533f5d8fb2321f5a221b
Dan Schafer <danfschafer@gmail.com>
2016-06-13 17:04:29 -0700

    finish up the form options in the index.html

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function() {
+  $("#vacationForm form").submit(function(event) {
+  var person1Input = $("input#person1").val();
+  var person2Input = $("input#person2").val();
+  var animalInput= $("input#animal").val();
+  var exclamationInput = $("input#exclamation").val();
+  var verbInput = $("input#verb").val();
+  var nounInput = $("input#noun").val();
+
+  event.preventDefault();
+}
