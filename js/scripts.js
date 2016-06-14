874707c02cb52f1279bf57506eb927b71ec835b8
Dan Schafer <danfschafer@gmail.com>
2016-06-13 22:49:41 -0700

    forms complete.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,32 @@
 $(function() {
   $("#vacationForm form").submit(function(event) {
-  var person1Input = $("input#person1").val();
-  var person2Input = $("input#person2").val();
-  var animalInput= $("input#animal").val();
-  var exclamationInput = $("input#exclamation").val();
-  var verbInput = $("input#verb").val();
-  var nounInput = $("input#noun").val();
+
+  var mntScore
+  var beachScore
+  var cityScore
+  var southScore
+
+  var name = $("input#user").val();
+  $(".user").text(name);
+
+  var foodChoice = $("input#food").val();
+  if (foodChoice==="1") {
+    mntScore +=1
+  if (foodChoice==="2") {
+    beachScore +=1
+  if (foodChoice==="3") {
+    cityScore +=1
+  if (foodChoice==="4") {
+    southScore +=1
+  }
+
+  var lodgingChoice= $("input#lodging").val();
+  var activityInput = $("input#activity").val();
+  var architectureInput = $("input#architecture").val();
+  var likesInput = $("input#likes").val();
+
+  $("#output").show();
 
   event.preventDefault();
-}
+  });
+});
